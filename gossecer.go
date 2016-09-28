555ea6763040200d7448a6ff4ecc3447327e0759
nohupped <nohupped@gmail.com>
2016-09-29 01:02:12 +0530

    UDPserver up and running

--- gossecer.go
+++ gossecer.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/nohupped/GoLogger"
-	"Ossecer/modules"
+	"Gossecer/modules"
 	"flag"
+	"sync"
 )
 
 func main() {
-	mylogger := GoLogger.New("/var/log/ossecer.log")
+	mylogger := GoLogger.New("/var/log/gossecer.log")
 	defer mylogger.Close()
 	configfileparam := flag.String("config", "/var/ossec/etc/ossec.conf", "Ossec main configuration file" +
 		" where syslog_output is defined.")
@@ -16,6 +17,8 @@ func main() {
 	host, ip :=modules.GetConfig(configfileparam)
 
 	mylogger.Info.Println("Starting UDP server on ", host, ip)
-
-	modules.StartUdpServer(host, ip, mylogger)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go modules.StartUdpServer(host, ip, wg)
+	wg.Wait()
 }
--- modules/startudpserver.go
+++ modules/startudpserver.go
@@ -4,12 +4,30 @@ import (
 	"github.com/nohupped/GoLogger"
 	"net"
 	"strconv"
-	"fmt"
+	"sync"
 )
+var udplogger *GoLogger.LogIt
 
-func StartUdpServer(host string, port int, mylogger *GoLogger.LogIt)  {
-	serveraddr, err := net.ResolveUDPAddr(host, strconv.Itoa(port))
+func StartUdpServer(host string, port int, wg *sync.WaitGroup)  {
+	udplogger = GoLogger.New("/var/log/gossecer_udp.log")
+	defer udplogger.Close()
+	serveraddr, err := net.ResolveUDPAddr("udp4", (host + ":" + strconv.Itoa(port)))
 	CheckError(err)
-	fmt.Println(serveraddr)
+	listner, err := net.ListenUDP("udp", serveraddr)
+	CheckError(err)
+	udplogger.Info.Println("Udp server listening on", serveraddr.String())
+	defer listner.Close()
+	for ; ;  {
+		handleUDP(listner)
+	}
+	wg.Done()
 
+}
+
+func handleUDP(conn *net.UDPConn) {
+	buffer := make([]byte, 1024)
+	n, addr, err := conn.ReadFromUDP(buffer)
+	udplogger.Info.Println("Client: ", addr)
+	CheckError(err)
+	udplogger.Info.Println(string(buffer[:n]))
 }
